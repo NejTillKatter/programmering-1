ac02d832dc816caf1e8b6f9b7c4e8ed5dbaa23db
NejTillKatter <alexander.ingnas@elev.ga.ntig.se>
2022-09-12 13:38:44 +0200

    w

---
+++ travelbag.py
@@ -0,0 +1,22 @@
+travelbag = ["tandborste", "kläder", "legitimation"]
+
+while True:
+
+   menyval = input("\n1. Kolla i resväskan\n"
+                   "2. Lägg sak i resväskan\n"
+                   "3. Ta bort sak i resväskan\n"
+                   "4. Avsluta program\n")
+
+   if menyval == "1":
+       print(*travelbag, sep=", ", ) 
+
+   elif menyval == "2":
+       more = input("väljett föremål att lägga till väskan")
+       travelbag.append(more)
+
+   elif menyval == "3":
+     less = input("välj ett föremål att ta bort")
+     travelbag.remove(less)
+
+   elif menyval == "4":
+       break
