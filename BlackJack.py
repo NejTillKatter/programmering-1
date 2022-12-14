ed126642c2cf7270ac7597a73f3c63a9c4aa37c6
NejTillKatter <alexander.ingnas@elev.ga.ntig.se>
2022-12-14 11:20:31 +0100

    wasd

--- BlackJack.py
+++ BlackJack.py
@@ -2,24 +2,40 @@ import random
 #skapa kortleken
 färger = [ "hjärter", "ruter", "spader", "klöver" ]
 
-kort = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13" ]
+kort = [ "ess", "2", "3", "4", "5", "6", "7", "8", "9", "10", "knekt", "dam", "kung" ]
+
+kortvärde = {"ess" : 11, "knekt" : 10, "dam" : 10, "kung" : 10}
 
 gekort = []
 
+huset = []
+
+#Gör så att listan gekort får 
 for färg in färger:
     for k in kort:
         gekort.append(färg + " " + k)
-print (gekort[])
 
- 
+#shuffla listan gekort
+
+random.shuffle(gekort)
+
+#definera och ge huset sina kort när koden kallas på
+for _ in range(3):
+    nytt_kort = gekort.pop(-1)
+    huset.append(nytt_kort)
+
+
+
 
 while True:
-    börja = input("vill du börja spela Black? Ja eller Nej\n:").capitalize()
+    #Input så man kan välja om man vill börja eller inte
+    börja = input("vill du börja spela Black Jack? Ja eller Nej\n:").capitalize()
     
 
     if börja == "Ja":
         #ger ut korten
         pass
+        
         
 
 
