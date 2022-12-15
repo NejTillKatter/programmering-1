2510823f6776504b0256fa685505b8155bae49e7
NejTillKatter <alexander.ingnas@elev.ga.ntig.se>
2022-12-15 14:38:32 +0100

    mr22

--- BlackJack.py
+++ BlackJack.py
@@ -11,22 +11,29 @@ def nya(gekort, kortlek):
 
 
 #Popar allt ifrån listan gekort
-def pop(gekort):
+def nya(datorkort, kortlek):
+
     for _ in range(3):
-        gekort.pop(-1)
+        nya_kort = kortlek.pop(-1)
+        datorkort.append(nya_kort)
 
 
 
-#Skapa kortleken
+#Gör två listor n med alla nummer på korten och en med alla färgerna
 färger = [ "hjärter", "ruter", "spader", "klöver" ]
 
 kort = [ "ess", "2", "3", "4", "5", "6", "7", "8", "9", "10", "knekt", "dam", "kung" ]
 
-kortvärde = {"ess" : 11, "knekt" : 10, "dam" : 10, "kung" : 10}
+#Ge korten värde så man kan räkna ut poäng efter
+kortvärde = {"ess" : 11, "knekt" : 10, "dam" : 10, "kung" : 10, "10" : 10, "9" : 9, "8" : 8, "7" : 7, "6" : 6, "5" : 5, "4" : 4, "3" : 3, "2" : 2}
 
 kortlek = []
 
 gekort = []
+
+datorkort = []
+
+
 
 #Gör så att listan kortlek får alla kort i en kortlek
 for färg in färger:
@@ -35,36 +42,40 @@ for färg in färger:
 
 
 
-#Shuffla listan kortlek
+#Shuffla listan kortlek så man inte får korten i en speciell ordning
 random.shuffle(kortlek)
 
 
 
 while True:
-    #Input så man kan välja om man vill börja eller inte
+    #Input så man kan välja om man vill börja eller inte 
+    #änvänds även i en loop så man kan köra flera gånger
     börja = input("vill du börja spela Black Jack? Ja eller Nej\n:").capitalize()
     
+
+
     #Om inputen börja är Ja händer det under indenten
     if börja == "Ja":
-        
+
         #Ger ut korten till gekort (handen)
         nya(gekort, kortlek)
 
         #Skriv ut korten i handen med , som separation
-        print(gekort, sep=", ")
+        print(gekort, "är dina kort")
         
-        #Kallar en funktion som tar bort allt från listan gekort
-        pop(gekort)
+        #Delar ut korten genom att kalla funktionen för att ge kort
+        nya(datorkort, kortlek)
 
-        #Delar ut datorns kort
+        print(datorkort)
 
-
-        
 
 
     elif börja == "Nej":
         
         #Avslutar programmet om man svarar nej på inputen börja
         break
+    
+    
+
     else:
         print("\nSkriv Ja eller nej\n")
