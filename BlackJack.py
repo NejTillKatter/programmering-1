8ffa704a89270fb2173b89d3d003d92083c346ec
NejTillKatter <alexander.ingnas@elev.ga.ntig.se>
2023-01-11 11:18:30 +0100

    wasd

--- BlackJack.py
+++ BlackJack.py
@@ -32,14 +32,25 @@ färger = [ "hjärter", "ruter", "spader", "klöver" ]
 
 kort = [ "ess", "2", "3", "4", "5", "6", "7", "8", "9", "10", "knekt", "dam", "kung" ]
 
-#Ge korten värde så man kan räkna ut poäng efter
-kortvärde = {"ess" : 11, "knekt" : 10, "dam" : 10, "kung" : 10, "10" : 10, "9" : 9, "8" : 8, "7" : 7, "6" : 6, "5" : 5, "4" : 4, "3" : 3, "2" : 2}
+
+
+#ger korten värde genom att splitta stringen i kortleken och ger värde utifrån kortet
+def handsumma(hand):
+    #Ge korten värde så man kan räkna ut poäng efter
+    kortvärde = {"ess" : 10, "knekt" : 10, "dam" : 10, "kung" : 10, "10" : 10, "9" : 9, "8" : 8, "7" : 7, "6" : 6, "5" : 5, "4" : 4, "3" : 3, "2" : 2}
+    score = 0
+    for card in hand:
+        value = card.split()[1]
+        score += kortvärde[value]
+
+    return score
 
 kortlek = []
 
 gekort = []
 
 datorkort = []
+
 
 
 
@@ -70,11 +81,28 @@ while True:
 
         #Skriv ut korten i handen med , som separation
         print(gekort, "är dina kort")
+        print("Värdet är", handsumma(gekort))
         
         #Delar ut korten genom att kalla funktionen för att ge kort
         nya(datorkort, kortlek)
 
         print(datorkort, "är husets kort")
+        print("värdet är", handsumma(datorkort))
+
+        if handsumma(gekort) == handsumma(datorkort):
+            print("Det blev lika")
+
+        elif handsumma(gekort) == 30 and handsumma(datorkort) != 30:
+            print("Vann")
+
+        elif handsumma(gekort) == 21 and handsumma(datorkort) !=21:
+            print("Vann")
+
+        elif handsumma(gekort) == 10 and handsumma(datorkort) != 10:
+            print("vann")
+
+        elif handsumma(gekort) > handsumma(datorkort):
+            print("Vann")
 
 
         pop(gekort, datorkort)
@@ -89,4 +117,4 @@ while True:
     
 
     else:
-        print("\nSkriv Ja eller nej\n")
+        print("\nSkriv Ja eller Nej\n")
